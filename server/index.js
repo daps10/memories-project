03a6d9c1bb0a0479953da30ed62b6114de1f9c06
Daps <52228501+daps10@users.noreply.github.com>
2023-02-20 21:13:18 +0530

    Start the base app.

--- server/index.js
+++ server/index.js
@@ -1,3 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+// middlewares
+app.use(express.json({ limit: '50mb' }));
+app.use(cors());
