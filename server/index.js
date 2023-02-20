e86d136718bd59f4fdfcead71f8cbb9b272f3adb
Daps <52228501+daps10@users.noreply.github.com>
2023-02-20 21:28:41 +0530

    Connect with mongoose

--- server/index.js
+++ server/index.js
@@ -7,3 +7,13 @@ const app = express();
 // middlewares
 app.use(express.json({ limit: '50mb' }));
 app.use(cors());
+
+const CONNECTION_URL= "mongodb+srv://daps:YpGY8a1Ow9rg5RZf@cluster0.nrn2f.mongodb.net/memoriesProject?retryWrites=true&w=majority";
+
+const PORT= process.env.PORT || 5000;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(CONNECTION_URL)
+.then(() => app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`)))
+.catch((error) => console.log(error.message));
+
