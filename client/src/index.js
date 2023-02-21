7d31eb7073369f7adea3f26925ac725158e4201e
Daps <52228501+daps10@users.noreply.github.com>
2023-02-21 22:08:23 +0530

    Initiated posts reducers.
    - Created two directories for actions and reducers to displays manages the reducers.
    - In posts reducers call the get posts api and return the response into the switch statements

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
+import { 
+    creatStore,
+    applyMiddleware,
+    compose,
+    compose
+} from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
+import reducers from './reducers'
+const store= creatStore(
+    reducers,
+    compose(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
     <App />,
