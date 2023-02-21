1e33fb15016a5d849e3b4d9b604bd15f018248c0
Daps <52228501+daps10@users.noreply.github.com>
2023-02-21 22:38:39 +0530

    Set a proxy into the package.json file.
    - Set a proxy in package.json file.
    - In posts component called the useSelector to manages its state
    - Resolved the createStore redux error

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { 
-    creatStore,
+    createStore,
     applyMiddleware,
-    compose,
     compose
 } from 'redux';
 import thunk from 'redux-thunk';
 import App from './App';
 import reducers from './reducers'
-const store= creatStore(
+
+const store= createStore(
     reducers,
     compose(applyMiddleware(thunk))
 );
