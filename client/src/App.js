c52c6c4c2b5ca7018d381e9b4f2cd17b644df980
Daps <52228501+daps10@users.noreply.github.com>
2023-02-21 21:42:55 +0530

    Created all the necessity styles in individuals folders.

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import {
     Grid 
 } from '@material-ui/core';
 
-import memories from './images/memories.jpg';
+import memories from './images/memories.png';
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import useStyles from './styles';
